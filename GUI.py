5c06d5a8b4c9d58de431d7704ee1988433bfa58e
Dorian Mutti <76829443+cl0udy69@users.noreply.github.com>
2024-03-04 09:16:28 -0500

    GUI.py

--- GUI.py
+++ GUI.py
@@ -9,25 +9,20 @@ class DataManagementSystem:
     def __init__(self, root):
         self.root = root
         self.root.title("Dorian's Data System")
-
-        # Variables for user input
+       
         self.domain_var = tk.StringVar()
         self.username_var = tk.StringVar()
         self.password_var = tk.StringVar()
 
-        # Existing saved data
         self.saved_logins = {}
         self.saved_address = {}
         self.saved_banking_info = {}
 
-        # Load saved data from file
         self.load_data_from_file()
 
-        # Create frames for different options
         self.frames = {}
         self.create_frames()
 
-        # Widgets
         self.create_widgets()
 
     entry_style = {'font': ('Arial', 12), 'bg': 'white', 'width': 20}
@@ -52,12 +47,10 @@ class DataManagementSystem:
             json.dump(data_to_save, file)
 
     def create_frames(self):
-        # Create frames for different options
         for option in ['Save Login', 'Generate Password', 'Save Personal Information', 'Save Banking Info', 'See All Saved Logins', 'See All saved Address\'s', 'See All Saved Banking Information', 'See All Saved Data']:
             self.frames[option] = tk.Frame(self.root)
 
     def create_widgets(self):
-        # Styling
         self.root.geometry("400x300")
         self.root.configure(bg="#f0f0f0")
 
@@ -65,28 +58,22 @@ class DataManagementSystem:
         button_style = {'font': ('Montserrat', 12), 'bg': '#4CAF50', 'fg': 'white', 'width': 15, 'height': 1}
 
 
-        # Labels
         tk.Label(self.root, text="Select an option:", **label_style).pack(pady=10)
 
-        # Buttons for different options
         options = ['Save Login', 'Generate Password', 'Save Personal Information', 'Save Banking Info', 'See All Saved Data']
         for option in options:
             tk.Button(self.root, text=option, command=lambda o=option: self.show_frame(o), **button_style).pack(pady=5)
 
-        # Place frames
         for frame in self.frames.values():
             frame.pack_forget()
 
-        # Initial frame to show
         self.show_frame('Save Login')
 
     def show_frame(self, option):
-        # Show the selected frame
         for frame in self.frames.values():
             frame.pack_forget()
         self.frames[option].pack(fill=tk.BOTH, expand=True)
 
-        # Handle specific frame content
         if option == 'Save Login':
             self.create_save_login_frame()
         elif option == 'Generate Password':
@@ -135,19 +122,38 @@ class DataManagementSystem:
         pass
 
     def save_address_frame(self):
-        # Your existing personal information code goes here
+        address = input('Street Adress: ')
+        state = input('State/province: ')
+        city = input('City: ')
+        zipcode = input('Zipcode: ')
+        aptnumber = input('Apartment Number: ')
+
+        self.saved_address[address] = {'state': state, 'city': city, 'zipcode': zipcode, 'aptnumber': aptnumber}
         pass
 
     def save_banking_info_frame(self):
-        # Your existing banking info code goes here
+        issuer = input('Card Issuer: ')
+        name = input('Card Name: ')
+        networks = input('Card Network: ')
+        account_opening = ('Date of account opening: ')
+        experiation = input('Experiation Date: ')
+        cardholder_name = input ('Cardholder Name: ')
+        number = input('Credit Card Number: ')
+        cvv = input('CVV (the three digits on the back of the card): ')
+
+        self.saved_banking_info[issuer] = {'name': name, 'networks': networks, 'account_opening': account_opening, 'experiation': experiation, 'cardholder_name': cardholder_name, 'number': number,  'cvv': cvv}
         pass
 
     def see_saved_logins(self):
-        # Your existing see all data code goes here
+        if not self.saved_logins:
+            print('No saved logins')
+        else:
+            print('Saved Logins')
+            for domain, login_info in self.saved_logins.items():
+                print(f'Street Address: {login_info["Street Address"]}, State: {login_info["State/province"]}, City: {login_info["City"]}, Zipcode: {login_info["Zipcode"]}, Apartment Number: {login_info["Apartment Number"]}')
         pass
 
     def see_saved_address(self):
-        # Your Existing see saved address code goes here
         pass
 
     def save_login(self):
@@ -155,13 +161,10 @@ class DataManagementSystem:
         username = self.username_var.get()
         password = self.password_var.get()
 
-        # Your existing code to save login information
         self.saved_logins[domain] = {'Username': username, 'Password': password}
 
-        # Inform the user
         messagebox.showinfo("Saved", "Login information saved successfully.")
 
-        # Save data to file
         self.save_data_to_file()
 
 
