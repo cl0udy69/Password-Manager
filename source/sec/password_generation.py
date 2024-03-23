b50bb9ae63c53281aab08ebd83bdbb908197b75d
Dorian Mutti <76829443+cl0udy69@users.noreply.github.com>
2024-03-23 15:45:27 -0400

    Update password_generation.py

---
+++ source/sec/password_generation.py
@@ -0,0 +1,21 @@
+import random
+
+def generate_passwords():
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$%^&*()_+-=[]{}|;:,.<>/?~'
+    number = int(input('Amount of passwords: '))
+    length = int(input('Length of password: '))
+    special = input('What special characters would you like to delete: ')
+    numbers = input('What numbers would you like to delete: ')
+    lowercase = input('What lowercase letters would you like to delete: ')
+    capitals = input('What capital letters would you like to delete: ')
+
+    for pwd in range(number):  
+        password = ''
+        for pwd in range(length):
+            char = random.choice(chars)
+            if char not in special and char not in numbers and char not in lowercase and char not in capitals:
+                password += char
+        print(password)
+        save_generated_password = input('Would you like to save this password (make sure to copy and paste the password)')
+        if save_generated_password in ['Yes', 'yes', 'Y', 'y', 'Save Password', 'Save password', 'save password']:
+            login_saver()
