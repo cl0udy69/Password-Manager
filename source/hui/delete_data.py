762bc25afc76af4a38852e65900b1f9d594eb1b3
Dorian Mutti <76829443+cl0udy69@users.noreply.github.com>
2024-03-25 08:22:31 -0400

    Update delete_data.py

--- source/hui/delete_data.py
+++ source/hui/delete_data.py
@@ -1,7 +1,114 @@
 def delete_address_field(address):
-    field_to_delete = input('Enter the field you would like to delete (e.g. State, Adress, Zipcode, Etc): ')
+    field_to_delete = input('Enter the field you would like to delete (e.g. State, Adress, Zipcode, Etc): ').lower()
     if field_to_delete in save_address[address]:
         del save_address[address][field_to_delete]
         print(f'{field_to_delete.capitalize()} deleted successfully')
     else:
         print('Field not found')
+        
+def delete_banking_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if fieled_to_delete in save_bankkng[banking]:
+        del save_banking[banking][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+
+def delete_ssn_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_ssn[ssn]:
+        del save_ssn[ssn][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted succesfully')
+    else:
+        print('Field not found')
+        
+def delete_phone_number_field():
+    field_to_delete = input('Enter the field you would like to delete').lower()
+    if field_to_delte in save_phone_number[phone_number]:
+        del save_phone_number[phone_number][field_to_delete]
+        print(f'{field_to_delete.capitalize()} delete succesfully')
+    else:
+        print('Field not found')
+        
+def delete_login_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_login[logni]:
+        del save_login[login][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_email_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_email[email]:
+        del save_email[email][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+    
+def delete_password_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_password[password]:
+        del save_password[password][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_birth_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_birth[birth]:
+        del save_birth[birth][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_gender_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_gender[gender]:
+        del save_gender[gender][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_nationality_field():
+    field_to_delete = input('Enter the fieled you would like to delete: ').lower()
+    if field_to_delete in save_nationality[nationality]:
+        del save_nationality[nationality][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_occupation_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_occupation[occupation]:
+        del save_occupation[occupation][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_education_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_education[education]:
+        del save_education[education][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+
+def delete_medical_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_medical[medical]:
+        del save_medical[medica][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_insurance_field():
+    field_to_delete = input('Enter the field you would like to delete: ').lower()
+    if field_to_delete in save_insurance[insurance]:
+        del save_insurance[insurance][field_to_delete]
+        print(f'{field_to_delete.capitalize()} deleted successfully')
+    else:
+        print('Field not found')
+        
+def delete_passport_field():
+    field_to_delete = input('Enter the field you would like to delete: ')
