e188dc421ef55e481df2b42cf7306fdcfbae2cfc
Dorian Mutti <76829443+cl0udy69@users.noreply.github.com>
2024-03-28 13:36:10 -0400

    Update notes.py

--- misc/notes/notes.py
+++ misc/notes/notes.py
@@ -12,6 +12,6 @@
 # ~ Update error handling for all functions 
 # ~ Combine all files into one main file for complete organization and so the code actually runs 
 # ~ Make the user selection and overall code less chunky and work more fluidly 
-# ~ Add more user choices to user selection prompts
+# ~ Add more user choices to user selection prompts (FINSIHED)
 # ~ Add more options and functionality 
 # ~ Clean up code 
