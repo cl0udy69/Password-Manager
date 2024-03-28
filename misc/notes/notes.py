960b3e2208a639a5504fca21480eb2b141b06261
Dorian Mutti <76829443+cl0udy69@users.noreply.github.com>
2024-03-27 21:21:47 -0400

    Update notes.py

---
+++ misc/notes/notes.py
@@ -0,0 +1,17 @@
+# ~ create loop to handle user input error validation
+# ~ organize see_data function when it comes to showing user responses 
+# ~ organize code overall
+# ~ turn into API and Discord bot 
+# ~ Make the code more user friendly 
+# ~ Impliment security protocls 
+# ~ Add verification system
+# ~ Allow users to have full control over data 
+# ~ Allow users to save genderated password directly to the save_password or save_login while still in the same generate_password function instead of just copy and pasting
+# ~ Finish password generation system entirely before persuing this ^^
+# ~ Update user selection prompts fully with adequate error handling
+# ~ Update error handling for all functions 
+# ~ Combine all files into one main file for complete organization and so the code actually runs 
+# ~ Make the user selection and overall code less chunky and work more fluidly 
+# ~ Add more user choices to user selection prompts
+# ~ Add more options and functionality 
+# ~ Clean up code 
