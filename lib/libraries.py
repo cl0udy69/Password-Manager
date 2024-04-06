df2aa3f3caed27e98e1e1b9e3ac5348f8b0ce2b1
Dorian Mutti <76829443+cl0udy69@users.noreply.github.com>
2024-04-06 13:50:10 -0400

    Update libraries.py

--- lib/libraries.py
+++ lib/libraries.py
@@ -1,3 +1,18 @@
-import random
+from address_function import address_selection
+from banking_function import banking_selection
+from ssn_function import ssn_selection
+from phone_number_function import phone_number_selection
+from login_function import login_selection
+from email_function import email_selection
+from password_function import password_selection
+from birth_function import birth_selection
+from gender_function import gender_selection
+from nationality_function import nationality_selection
+from occupation_function import occupation_selection
+from education_function import education_selection
+from medical_function import medical_selection
+from insurance_function import insurance_selection
+from passport_function import passport_selection
+from legal_function import legal_selection
+from ethnicity_function import ethnicity_selection
 import json
-import os
